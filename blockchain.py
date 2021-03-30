fbb69347b4d7f73c6e0573a191c7a2a49ca1f1bf
lawellwilhelm <lawellwilhelm.dev@gmail.com>
2021-03-30 17:30:24 +0000

    Implement open transaction verification functionality

--- blockchain.py
+++ blockchain.py
@@ -69,12 +69,13 @@ def mine_block():
         'amount': MINING_REWARD
     }
 
-    open_transactions.append(reward_transaction)
+    copied_transactions = open_transactions[:]
+    copied_transactions.append(reward_transaction)
     
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
-        'transactions': open_transactions
+        'transactions': copied_transactions
     }
     blockchain.append(block)
     return True
@@ -143,6 +144,10 @@ def verify_blockchain():
             return False
     
     return True
+
+
+def verify_transactions():
+    return all([verify_transaction(tx) for tx in open_transactions])
     
 
 waiting_for_input = True
@@ -153,6 +158,7 @@ while waiting_for_input:
     print('2: Mine a new block')
     print('3: Output the transaction blocks')
     print('4: Output participants')
+    print('5: Check transaction validity')
     print('h: Manipulate the chain')
     print('q: Quit')
 
@@ -172,6 +178,11 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == '4':
         print(participants)
+    elif user_choice == '5':
+        if verify_transactions():
+            print('All transactions are valid')
+        else:
+            print('There are invalid transactions')
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
